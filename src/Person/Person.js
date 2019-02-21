2064ea432e65707570df71bafd23a48d60ecb613
Ryan Freeman <ryanfreeman@Ryans-iMac.local>
2019-02-21 15:44:41 -0700

    added license

--- src/Person/Person.js
+++ src/Person/Person.js
@@ -3,7 +3,7 @@ import React from 'react'
 const Person = () => {
     return (
         <p>
-            I'm a person!
+            I'm a person! and I am {Math.floor(Math.random() * 30)} years old
         </p>
     )
 }
