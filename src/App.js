57b6120c4281545ab0ab8ebab21ba6c78fb75d14
Ryan Freeman <ryanfreeman@Ryans-iMac.local>
2019-02-21 16:55:54 -0700

    additional commits

--- src/App.js
+++ src/App.js
@@ -3,14 +3,34 @@ import './App.css';
 import Person from './Person/Person'
 
 class App extends Component {
+  state = {
+    persons: [
+      { name: 'Ryan', age: 28},
+      { name: 'Lucy', age: 2},
+      { name: 'Rachael', age: 23}
+    ]
+  }
+
+  switchHandler = () => {
+    //Don't do this this.state.persons[0].name = 'Ryan the beautiful'
+    this.setState({
+      persons: [
+        { name: 'Ryan the beautiful', age: 28},
+        { name: 'Lucy the brave', age: 2},
+        { name: 'Rachael the pretty', age: 23}
+      ]
+    })
+  }
+
   render() {
     return (
       <div className="App">
         <h1>Hi, I'm a React App</h1>
         <p>This is really working!</p>
-        <Person />
-        <Person />
-        <Person />
+        <button onClick={this.switchHandler}>Switch Name</button>
+        <Person name={this.state.persons[0].name} age={this.state.persons[0].age}>My hobbies: wedding planning</Person>
+        <Person name={this.state.persons[1].name} age={this.state.persons[1].age}>My hobbies: wedding planning</Person>
+        <Person name={this.state.persons[2].name} age={this.state.persons[2].age}>My hobbies: wedding planning</Person>
       </div>
     );
     // return React.createElement('div', {className: 'App'}, React.createElement('h1', null, 'Hi, I\'m a React App!'))
--- src/Person/Person.js
+++ src/Person/Person.js
@@ -1,10 +1,12 @@
 import React from 'react'
 
-const Person = () => {
+const Person = (props) => {
     return (
-        <p>
-            I'm a person! and I am {Math.floor(Math.random() * 30)} years old
-        </p>
+        <div>
+            <p>My name is {props.name}, and I am {props.age} years old</p>
+            <p>{props.children}</p>
+        </div>
+        
     )
 }
 
